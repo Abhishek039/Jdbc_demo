85d6504197244748ff838ee08d1ea1e692aaa012
Abhishek Pandey <75622752+Abhishek039@users.noreply.github.com>
2021-02-27 12:52:00 +0530

    Update JdbcDemo.java

--- src/jdbcdemo/JdbcDemo.java
+++ src/jdbcdemo/JdbcDemo.java
@@ -20,35 +20,37 @@ public class JdbcDemo {
       Connection conn =DriverManager.getConnection("jdbc:mysql://localhost:3306/st", "root", "");
     
       // create a sql date object so we can use it in our INSERT statement
-      Calendar calendar = Calendar.getInstance();
-      java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+      
 
       // the mysql insert statement
-      //String query = " insert into student (rollno, sname)" + " values (?, ?)";
+      String query = " insert into student (rollno, sname)" + " values (?, ?)";
 
       // create the mysql insert preparedstatement
-      //PreparedStatement preparedStmt = conn.prepareStatement(query);
-      //preparedStmt.setInt (1, 6);
-      //preparedStmt.setString (2, "Rubble");
+      PreparedStatement preparedStmt = conn.prepareStatement(query);
+      preparedStmt.setInt (1, 6);
+      preparedStmt.setString (2, "Rubble");
       
-      //String query1 = "update student set sname = ? where rollno= ?";
-      //PreparedStatement preparedStmt1 = conn.prepareStatement(query1);
-      //preparedStmt1.setInt   (1, 3);
-      //preparedStmt1.setInt (2, 4);
+      String query1 = "update student set sname = ? where rollno= ?";
+      PreparedStatement preparedStmt1 = conn.prepareStatement(query1);
+      preparedStmt1.setInt   (1, 3);
+      preparedStmt1.setInt (2, 4);
 
 
 
       // execute the preparedstatement
-      //preparedStmt1.execute();
-     /** Statement stmt= conn.createStatement();  
+      preparedStmt1.execute();
+      Statement stmt= conn.createStatement();  
     ResultSet rs=stmt.executeQuery("select rollno, sname from student where rollno =2");  
     
     while(rs.next())
    {
    System.out.println(rs.getString(1));   
     System.out.println(rs.getString(2));
-   }**/
-      String sql = "DELETE FROM student WHERE rollno=?";
+   }
+    
+      
+      //delete action performed
+  String sql = "DELETE FROM student WHERE rollno=?";
  
  PreparedStatement statement = conn.prepareStatement(sql);
  statement.setInt(1,6000 );
